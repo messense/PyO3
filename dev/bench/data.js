ebe2ed14ee472394723a84c4b86c5267d7779635
github-action-benchmark <github@users.noreply.github.com>
2021-05-26 11:07:44 +0000

    add bench_dict (cargo) benchmark result for 0f249dd61315e74a97f052d90f532a388f051835

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1622027209075,
+  "lastUpdate": 1622027263259,
   "repoUrl": "https://github.com/messense/pyo3",
   "entries": {
     "bench_pyobject": [
@@ -269,6 +269,62 @@ window.BENCHMARK_DATA = {
             "name": "tuple_get_item",
             "value": 754333,
             "range": "± 16998",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ],
+    "bench_dict": [
+      {
+        "commit": {
+          "author": {
+            "email": "messense@icloud.com",
+            "name": "messense",
+            "username": "messense"
+          },
+          "committer": {
+            "email": "messense@icloud.com",
+            "name": "messense",
+            "username": "messense"
+          },
+          "distinct": true,
+          "id": "0f249dd61315e74a97f052d90f532a388f051835",
+          "message": "Add automated continuous benchmarking",
+          "timestamp": "2021-05-26T19:01:53+08:00",
+          "tree_id": "1aef1933cd529bd002279f658af414586fee2f30",
+          "url": "https://github.com/messense/pyo3/commit/0f249dd61315e74a97f052d90f532a388f051835"
+        },
+        "date": 1622027246341,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "iter_dict",
+            "value": 2742847,
+            "range": "± 204362",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "dict_get_item",
+            "value": 3303157,
+            "range": "± 281703",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "extract_hashmap",
+            "value": 10114582,
+            "range": "± 1258468",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "extract_btreemap",
+            "value": 12665653,
+            "range": "± 937445",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "extract_hashbrown_map",
+            "value": 7990606,
+            "range": "± 733011",
             "unit": "ns/iter"
           }
         ]
