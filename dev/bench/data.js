c2f828d183c3b881818eb0f96169b6d9b58765eb
github-action-benchmark <github@users.noreply.github.com>
2021-05-26 11:16:42 +0000

    add bench_set (cargo) benchmark result for 0f249dd61315e74a97f052d90f532a388f051835

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1622027788057,
+  "lastUpdate": 1622027801799,
   "repoUrl": "https://github.com/messense/pyo3",
   "entries": {
     "bench_pyobject": [
@@ -257,6 +257,54 @@ window.BENCHMARK_DATA = {
             "name": "extract_hashbrown_set",
             "value": 4762824,
             "range": "± 144647",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "messense@icloud.com",
+            "name": "messense",
+            "username": "messense"
+          },
+          "committer": {
+            "email": "messense@icloud.com",
+            "name": "messense",
+            "username": "messense"
+          },
+          "distinct": true,
+          "id": "0f249dd61315e74a97f052d90f532a388f051835",
+          "message": "Add automated continuous benchmarking",
+          "timestamp": "2021-05-26T19:01:53+08:00",
+          "tree_id": "1aef1933cd529bd002279f658af414586fee2f30",
+          "url": "https://github.com/messense/pyo3/commit/0f249dd61315e74a97f052d90f532a388f051835"
+        },
+        "date": 1622027785025,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "iter_set",
+            "value": 2104299,
+            "range": "± 60362",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "extract_hashset",
+            "value": 10134834,
+            "range": "± 264933",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "extract_btreeset",
+            "value": 11725945,
+            "range": "± 346717",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "extract_hashbrown_set",
+            "value": 5986078,
+            "range": "± 178084",
             "unit": "ns/iter"
           }
         ]
