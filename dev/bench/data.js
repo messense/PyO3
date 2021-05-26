b1292d2566e1d0073b08e5e78344953b8237a1f6
github-action-benchmark <github@users.noreply.github.com>
2021-05-26 11:06:29 +0000

    add bench_pyclass (cargo) benchmark result for 0f249dd61315e74a97f052d90f532a388f051835

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1622027161813,
+  "lastUpdate": 1622027188193,
   "repoUrl": "https://github.com/messense/pyo3",
   "entries": {
     "bench_pyobject": [
@@ -29,6 +29,38 @@ window.BENCHMARK_DATA = {
             "name": "drop_many_objects",
             "value": 3827,
             "range": "± 148",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ],
+    "bench_pyclass": [
+      {
+        "commit": {
+          "author": {
+            "email": "messense@icloud.com",
+            "name": "messense",
+            "username": "messense"
+          },
+          "committer": {
+            "email": "messense@icloud.com",
+            "name": "messense",
+            "username": "messense"
+          },
+          "distinct": true,
+          "id": "0f249dd61315e74a97f052d90f532a388f051835",
+          "message": "Add automated continuous benchmarking",
+          "timestamp": "2021-05-26T19:01:53+08:00",
+          "tree_id": "1aef1933cd529bd002279f658af414586fee2f30",
+          "url": "https://github.com/messense/pyo3/commit/0f249dd61315e74a97f052d90f532a388f051835"
+        },
+        "date": 1622027171762,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "first_time_init",
+            "value": 5111,
+            "range": "± 22545",
             "unit": "ns/iter"
           }
         ]
