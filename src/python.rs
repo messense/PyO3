5c94e9da575e4f712534101dadd385863b811ef8
messense <messense@icloud.com>
2021-07-05 11:12:57 +0800

    Change `PyErr::fetch` to return an `Option<PyErr>`

--- src/python.rs
+++ src/python.rs
@@ -358,7 +358,7 @@ impl<'p> Python<'p> {
         unsafe {
             let mptr = ffi::PyImport_AddModule("__main__\0".as_ptr() as *const _);
             if mptr.is_null() {
-                return Err(PyErr::fetch(self));
+                return Err(PyErr::api_call_failed(self));
             }
 
             let globals = globals
@@ -368,7 +368,7 @@ impl<'p> Python<'p> {
 
             let code_obj = ffi::Py_CompileString(code.as_ptr(), "<string>\0".as_ptr() as _, start);
             if code_obj.is_null() {
-                return Err(PyErr::fetch(self));
+                return Err(PyErr::api_call_failed(self));
             }
             let res_ptr = ffi::PyEval_EvalCode(code_obj, globals, locals);
 
@@ -598,7 +598,7 @@ impl<'p> Python<'p> {
     pub fn check_signals(self) -> PyResult<()> {
         let v = unsafe { ffi::PyErr_CheckSignals() };
         if v == -1 {
-            Err(PyErr::fetch(self))
+            Err(PyErr::api_call_failed(self))
         } else {
             Ok(())
         }
